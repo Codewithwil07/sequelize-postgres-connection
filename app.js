4c0694528b349bfcddcb97080473daff9498a292
wilywangceo@gmail.com <wilywangceo@gmail.com>
2024-05-26 13:50:38 +0700

    end

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import bcrypt from 'bcrypt';
 import User from './user.js';
 
 const create = async () => {
-  const username = 'shenjuanwil';
-  const email = 'shenjuanwil@gmail.com';
+  const username = 'zhanglinghe';
+  const email = 'zhanglinghe@gmail.com';
   const password = bcrypt.hashSync('secret', 8);
 
   const user = await User.create({
@@ -14,8 +14,33 @@ const create = async () => {
 
   console.log(user);
 };
-const read = () => {};
-const update = () => {};
-const deleteData = () => {};
 
-create();
+const read = async () => {
+  const users = await User.findAll();
+  console.log(users);
+};
+
+const update = async () => {
+  const username = 'sabrinazhuang';
+  const email = 'sabrinazhuang@gmail.com';
+  const password = bcrypt.hashSync('secret', 8);
+
+  const user = await User.update(
+    {
+      username,
+      email,
+      password,
+    },
+    { where: { id: 2 } }
+  );
+  console.log(user);
+};
+
+const deleteData = async () => {
+  const user = await User.destroy({ where: { id: 2 } });
+};
+
+// create();
+// read()
+// update()
+deleteData()
